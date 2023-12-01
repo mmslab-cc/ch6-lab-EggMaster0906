d83a8af46ddba3ebf6358b1808b5466ffebae7d8
EggMaster0906 <lsinpen@gmail.com>
2023-12-01 14:25:50 +0800

    lab

--- ch6-lab1/source/main.c
+++ ch6-lab1/source/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../include/Information.h"
 
 int main(void)
 {
+	Person person;
+	printf("Please input your name: ");
+	scanf("%s", &person.name);
+
+	printf("Please input your gender<0 for woman, 1 for man> : ");
+	scanf("%d", &person.gender);
+
+	printf("Please input your age: ");
+	scanf("%d", &person.age);
+
+	printf("\nHi! %s\n", person.name);
+
+	if (person.gender == 0)
+	{
+		printf("Your are %d-year-old woman.\n", person.age);
+	}
+	else
+	{
+		printf("Your are %d-year-old man.\n", person.age);
+	}
+	
+	system("pause");
 	return 0;
 }
 
